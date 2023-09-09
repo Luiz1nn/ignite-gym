1532d9592a13eee93a0eadab06b9e3d124c5a6f8
Luis Fernando <luisfernandobr2003@gmail.com>
2023-09-09 01:52:04 -0300

    chore: add native base

--- App.tsx
+++ App.tsx
@@ -5,25 +5,28 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+import { NativeBaseProvider } from 'native-base'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#202024',
-      }}
-    >
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Text>Hello World</Text> : <View />}
-    </View>
+    <NativeBaseProvider>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#202024',
+        }}
+      >
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Text>Hello World</Text> : <View />}
+      </View>
+    </NativeBaseProvider>
   )
 }
