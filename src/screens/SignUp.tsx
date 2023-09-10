d7bc4e6c956f4f241736668c914de9a9c947ae6b
Luis Fernando <luisfernandobr2003@gmail.com>
2023-09-10 20:48:02 -0300

    feat(SignUp): add navigation

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,50 +1,62 @@
 import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base'
 import { BackgroundImg, LogoSvg } from '@/assets'
 import { Button, Input } from '@/components'
+import { useNavigation } from '@react-navigation/native'
 
-export const SignUp = () => (
-  <ScrollView
-    contentContainerStyle={{ flexGrow: 1 }}
-    showsVerticalScrollIndicator={false}
-  >
-    <VStack flex={1} px={10}>
-      <Image
-        alt="Pessoas treinando"
-        source={BackgroundImg}
-        defaultSource={BackgroundImg}
-        resizeMode="contain"
-        position="absolute"
-      />
+export const SignUp = () => {
+  const navigation = useNavigation()
 
-      <Center my={24}>
-        <LogoSvg />
+  const handleGoBack = () => navigation.goBack()
 
-        <Text color="gray.100" fontSize="sm">
-          Treine sua mente e o seu corpo.
-        </Text>
-      </Center>
-
-      <Center>
-        <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
-          Crie sua conta
-        </Heading>
-
-        <Input placeholder="Nome" />
-
-        <Input
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCapitalize="none"
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} px={10}>
+        <Image
+          alt="Pessoas treinando"
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          resizeMode="contain"
+          position="absolute"
         />
 
-        <Input placeholder="Senha" secureTextEntry />
+        <Center my={24}>
+          <LogoSvg />
 
-        <Input placeholder="Confirmar a Senha" secureTextEntry />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo.
+          </Text>
+        </Center>
 
-        <Button title="Criar e acesssar" />
-      </Center>
+        <Center>
+          <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
+            Crie sua conta
+          </Heading>
 
-      <Button title="Voltar para o login" variant="outline" mt={12} />
-    </VStack>
-  </ScrollView>
-)
+          <Input placeholder="Nome" />
+
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" secureTextEntry />
+
+          <Input placeholder="Confirmar a Senha" secureTextEntry />
+
+          <Button title="Criar e acesssar" />
+        </Center>
+
+        <Button
+          title="Voltar para o login"
+          variant="outline"
+          mt={12}
+          onPress={handleGoBack}
+        />
+      </VStack>
+    </ScrollView>
+  )
+}
