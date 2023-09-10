4dada641bd00e0fffbd7b2bca377e6c186f79d9e
Luis Fernando <luisfernandobr2003@gmail.com>
2023-09-10 17:12:16 -0300

    feat(SignIn): add Inputs

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,9 @@
 import { BackgroundImg, LogoSvg } from '@/assets'
-import { Center, Image, Text, VStack } from 'native-base'
+import { Input } from '@/components'
+import { Center, Heading, Image, Text, VStack } from 'native-base'
 
 export const SignIn = () => (
-  <VStack flex={1} bg="gray.700">
+  <VStack flex={1} bg="gray.700" px={10}>
     <Image
       alt="Pessoas treinando"
       source={BackgroundImg}
@@ -17,6 +18,19 @@ export const SignIn = () => (
       <Text color="gray.100" fontSize="sm">
         Treine sua mente e o seu corpo
       </Text>
+    </Center>
+
+    <Center>
+      <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+        Acesse a conta
+      </Heading>
+
+      <Input
+        placeholder="E-mail"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <Input placeholder="Senha" secureTextEntry />
     </Center>
   </VStack>
 )
