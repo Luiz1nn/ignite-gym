450b359b6efd7469b5bb8de103201a8f88bdf532
Luis Fernando <luisfernandobr2003@gmail.com>
2023-09-10 16:03:24 -0300

    feat(App): add Loading component

--- App.tsx
+++ App.tsx
@@ -1,32 +1,25 @@
 /* eslint-disable camelcase */
-import { StatusBar, Text, View } from 'react-native'
+import { StatusBar, View } from 'react-native'
+import { Loading } from '@/components'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+import { THEME } from '@/theme'
 import { NativeBaseProvider } from 'native-base'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <NativeBaseProvider>
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#202024',
-        }}
-      >
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        {fontsLoaded ? <Text>Hello World</Text> : <View />}
-      </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   )
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+import { Center, Spinner } from 'native-base'
+
+export const Loading = () => (
+  <Center flex={1} bg="grqy.700">
+    <Spinner color="green.500" />
+  </Center>
+)
