7b69da290eec769deb81ff7765a243427da5bcdd
Luis Fernando <luisfernandobr2003@gmail.com>
2023-09-17 12:30:19 -0300

    feat(Home): add ExerciseCard component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,61 @@
-import { VStack } from 'native-base'
+import { useState } from 'react'
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
+import ExerciseCard from './ExerciseCard'
+import Group from './Group'
 import Header from './Header'
 
-export const Home = () => (
-  <VStack flex={1}>
-    <Header />
-  </VStack>
-)
+export const Home = () => {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [exercises, setExercises] = useState([
+    'Puxada frontal',
+    'Remada curvada',
+    'Remada unilateral',
+    'Levantamento terra',
+  ])
+  const [groupSelected, setGroupSelected] = useState('Costas')
+
+  return (
+    <VStack flex={1}>
+      <Header />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={() => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
+    </VStack>
+  )
+}
