5eda0a7c6dba7b5cae206ff1232deb7e54621529
Luis Fernando <luisfernandobr2003@gmail.com>
2023-09-19 16:24:50 -0300

    feat(Profile): add ScreenHeader component

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,3 +1,8 @@
+import { ScreenHeader } from '@/components'
 import { VStack } from 'native-base'
 
-export const Profile = () => <VStack flex={1}></VStack>
+export const Profile = () => (
+  <VStack flex={1}>
+    <ScreenHeader title="Perfil" />
+  </VStack>
+)
