ff78c3c64e8d8bc82d16769b2f9bd46964a4268c
Luis Fernando <luisfernandobr2003@gmail.com>
2023-09-19 16:40:04 -0300

    feat(Profile): add Skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,17 +1,34 @@
-import { VStack, ScrollView, Center } from 'native-base'
+import { VStack, ScrollView, Center, Skeleton } from 'native-base'
 import { ScreenHeader, UserPhoto } from '@/components'
+import { useState } from 'react'
 
-export const Profile = () => (
-  <VStack flex={1}>
-    <ScreenHeader title="Perfil" />
-    <ScrollView>
-      <Center mt={6} px={10}>
-        <UserPhoto
-          source={{ uri: 'https://github.com/Luiz1nn.png' }}
-          alt="Foto do usuário"
-          size={33}
-        />
-      </Center>
-    </ScrollView>
-  </VStack>
-)
+const PHOTO_SIZE = 33
+
+export const Profile = () => {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/Luiz1nn.png' }}
+              alt="Foto do usuário"
+              size={33}
+            />
+          )}
+        </Center>
+      </ScrollView>
+    </VStack>
+  )
+}
