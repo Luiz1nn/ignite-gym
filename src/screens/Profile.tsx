619c91f3aad474affbae484ef7bdc304e94f81e1
Luis Fernando <luisfernandobr2003@gmail.com>
2023-09-19 16:31:01 -0300

    feat(Profile): add UserPhoto component

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,8 +1,17 @@
-import { ScreenHeader } from '@/components'
-import { VStack } from 'native-base'
+import { VStack, ScrollView, Center } from 'native-base'
+import { ScreenHeader, UserPhoto } from '@/components'
 
 export const Profile = () => (
   <VStack flex={1}>
     <ScreenHeader title="Perfil" />
+    <ScrollView>
+      <Center mt={6} px={10}>
+        <UserPhoto
+          source={{ uri: 'https://github.com/Luiz1nn.png' }}
+          alt="Foto do usuário"
+          size={33}
+        />
+      </Center>
+    </ScrollView>
   </VStack>
 )
