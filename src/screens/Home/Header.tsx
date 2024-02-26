27fd202e3ee068c45e5ba9401a4b461f784efe31
Luis Fernando <luisfernandobr2003@gmail.com>
2024-02-26 15:38:26 -0300

    feat(home): add name user

--- src/screens/Home/Header.tsx
+++ src/screens/Home/Header.tsx
@@ -2,30 +2,35 @@ import { TouchableOpacity } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 import { HStack, Heading, Icon, Text, VStack } from 'native-base'
 import { UserPhoto } from '@/components'
+import { useAuth } from '@/hooks'
 
-const Header = () => (
-  <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
-    <UserPhoto
-      source={{ uri: 'https://github.com/Luiz1nn.png' }}
-      size={16}
-      alt="Imagem do usuário"
-      mr={4}
-    />
+const Header = () => {
+  const { user } = useAuth()
 
-    <VStack flex={1}>
-      <Text color="gray.100" fontSize="md">
-        Olá,
-      </Text>
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: 'https://github.com/Luiz1nn.png' }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
 
-      <Heading color="gray.100" fontSize="md" fontFamily="heading">
-        Luis
-      </Heading>
-    </VStack>
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
 
-    <TouchableOpacity>
-      <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
-    </TouchableOpacity>
-  </HStack>
-)
+        <Heading color="gray.100" fontSize="md" fontFamily="heading">
+          {user.name}
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  )
+}
 
 export default Header
